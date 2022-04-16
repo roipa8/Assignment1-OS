80aa54c450ca33c6e28e738f6b5ee6ab71497f13
avishayts <77344388+avishayts@users.noreply.github.com>
2022-04-16 12:47:25 +0300

    Update test.c
    sjf

--- user/test.c
+++ user/test.c
@@ -36,9 +36,43 @@ void example_kill_system(int interval, int loop_size) {
     printf("\n");
 }
 
+void env(int size, int interval, char* env_name) {
+    int result = 1;
+    int loop_size = (int)10e6;
+    int n_forks = 2;
+    int pid;
+    for (int i = 0; i < n_forks; i++) {
+        pid = fork();
+    }
+    for (int i = 0; i < loop_size; i++) {
+        if (i % (int) (loop_size / 10e0) == 0) {
+        	if (pid == 0) {
+        		printf("%s %d/%d completed.\n", env_name, i, loop_size);
+        	} else {
+        		printf(" ");
+        	}
+        }
+        if (i % interval == 0) {
+            result = result * size;
+        }
+    }
+    printf("\n");
+}
+
+void env_large() {
+    env(10e6, 10e6, "env_large");
+}
+
+void env_freq() {
+    env(10e1, 10e1, "env_freq");
+}
+
+
 
 int main(int argc, char** argv){
-    example_pause_system(10,7,100);
-    example_kill_system(10,100);
+    env_freq();
+    env_large();
+    // example_pause_system(10,7,100);
+    // example_kill_system(10,100);
     exit(0);
 }
